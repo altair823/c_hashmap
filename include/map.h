0ef8bf9fec78f500540e33f3913e53b561448043
altair823 <dlsrks0734@gmail.com>
2023-02-13 10:08:21 +0900

    implement linked list node functions for bucket

--- include/map.h
+++ include/map.h
@@ -1,5 +1,22 @@
 #include <string.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct _node_t {
+    void *data;
+    struct _node_t *next_node;
+} node_t;
+
+node_t *create_linked_list();
+node_t *create_node(void *data);
+void pushback_node(node_t *head, void *data);
 
 
+typedef struct {
+    node_t *linked_list;
+    size_t length;
+} bucket_t;
 
+bucket_t *create_bucket();
+bool pushback(bucket_t *bucket);
--- src/map.c
+++ src/map.c
@@ -1,2 +1,37 @@
 #include <map.h>
 
+node_t *create_linked_list() {
+    node_t *head = NULL;
+    return head;
+}
+
+node_t *create_node(void *data) {
+    node_t *new_node = (node_t *)malloc(sizeof(node_t));
+    new_node->data = data;
+    new_node->next_node = NULL;
+    return new_node;
+}
+
+void pushback_node(node_t *head, void *data) {
+    // If linked list is empty,
+    if (head == NULL) {
+	head = create_node(data);
+    }
+    // or, if linked list is not empty.
+    else {
+	node_t *cur = head;
+	while (cur->next_node != NULL) {
+	    cur = cur->next_node;
+	}
+	node_t *new_tail = create_node(data);
+	cur->next_node = new_tail;
+    }
+}
+
+
+bucket_t *create_bucket() {
+    bucket_t *new_bucket = (bucket_t *)malloc(sizeof(bucket_t));
+    new_bucket->linked_list = create_linked_list();
+    new_bucket->length = 0;
+    return new_bucket;
+}
